17bbec60e8ed216469f046751a531beacf69bbb4
Shelbot77 <alessiodeangelis021@gmail.com>
2022-12-12 16:37:57 +0100

    Bonus commit

--- myscript.js
+++ myscript.js
@@ -3,7 +3,8 @@ const {createApp} = Vue
 createApp({
     data() {
         return{
-            message: 'Che bello. oggi si inizia Vue!!!'
+            message: 'Che bello. oggi si inizia Vue!!!',
+            myImage:  "https://images.everyeye.it/img-notizie/the-batman-nuovo-grosso-importante-update-futuro-saga-robert-pattinson-v3-623393-1280x960.webp"
         }
 
     }
